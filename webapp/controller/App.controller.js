fb2322b9dadf233e9e690120d5703fb744c889bc
kevin-gram <k.nyiringan@alustudent.com>
2024-11-21 11:22:35 +0200

    loading the json

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,16 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
     "sap/m/MessageBox",
     "sap/m/MessageToast"
-], (Controller, MessageBox, MessageToast) => {
+], function(Controller, JSONModel, MessageBox, MessageToast) {
     "use strict";
-    return Controller.extend("ui5.walkthrough.Controller.App", {
-        onPress() {
-            MessageToast.show("Here I am again Buddy!...")
-        }, 
-        onPost(oEvent) {
-            const oValue = oEvent.getSource().getValue();
-            MessageBox.error(oValue || "There is no message for you already");
+
+    return Controller.extend("ui5.walkthrough.controller.App", {
+     onInit(){
+        const oModel=new JSONModel()
+        oModel.loadData("model/data.json")
+        .then(()=>console.log("there was somthing wrong loading the data"))
+        .catch((error)=>console.error("There was an error ",error))
+        this.getView().setModel(oModel)
+     },
+        onPress: function() {
+            MessageToast.show("Button Pressed!");
+            console.log()
+        },
+        onPost: function(oEvent) {
+            const sValue = oEvent.getSource().getValue();
+            MessageBox.information(sValue || "No input provided.");
         }
     });
 });
